ba1fdf9e56a081406ae671872ad0ee500e513e5d
chriss1525 <okothchris15@gmail.com>
2023-03-08 16:00:21 +0300

    add intro sequence

--- language.c
+++ language.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h> 
 
 #define MAX_WORD 20
 
@@ -16,15 +17,22 @@ int main(void)
 	char *verb = malloc(sizeof(MAX_WORD));
 	char *object = malloc(sizeof(MAX_WORD));
 	char *simple_sentence;
-	char *tempverb = NULL;
-	char *tempobject = NULL;
 	char *random = malloc(sizeof(MAX_WORD));
 	char *temp_verb = NULL;
 	char *temp_object = NULL;
 
-	char *intro = "****Write a simple English sentence****";
-
-	printf("%s\n", intro);
+	printf("**There are several ways to write a sentence in the English Language**\n");
+	sleep(2);
+	printf("**The simplest of these is the SVO method where you only require three parts:**\n");
+	sleep(2);
+	printf("***The subject, often a noun but can be replaced with a pronoun or a subject clause***\n");
+	sleep(2);
+	printf("****the verb, often an action like make or break but can also be replaced by a verb clause****\n");
+	sleep(2);
+	printf("*****the object, where the action is directed. Can be replaced by an object clause*****\n");
+	sleep(2);
+	printf("LETS MAKE AN SVO SENTENCE\n");
+	sleep(1);
 	printf("**type any noun**\n");
 	scanf("%s", subject);
 	printf("**randomize word? (yes/no)**\n");
@@ -72,5 +80,6 @@ int main(void)
 	simple_sentence = concatenate(subject, verb, object);
 
 	printf("%s\n", simple_sentence);
+	printf("*** congratulations! You did it!***\n");
 	return (0);
 }
