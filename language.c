8c6807453d606794a20da20f18d86772208fa2f6
chriss1525 <okothchris15@gmail.com>
2023-03-08 12:22:35 +0300

    create file for ToF function

--- language.c
+++ language.c
@@ -72,24 +72,3 @@ int main(void)
 
 	printf("%s\n", simple_sentence);
 }
-
-
-int true_or_false(char *n)
-{
-	int ToF;
-
-	if (strcmp(n, "yes") == 0)
-	{
-		ToF = 1;
-	}
-	else if (strcmp(n, "no") == 0)
-	{
-		ToF = 0;
-	}
-	else
-	{
-		printf("Invalid input.\n");
-	}
-
-	return (ToF);
-}
